374a1425c87a4d164c5d07f9134d274ea688fbc4
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-12-22 06:08:33 -0600

    Write a simple JS Stalker Exercise

--- JavaScript_Basics/scriptDemo.js
+++ JavaScript_Basics/scriptDemo.js
@@ -1,3 +1,6 @@
-let userName = prompt("What's your name?");
-alert("Nice to meet you " + userName);
-console.log('Also nice to meet you ' + userName);
+let firstName = prompt("What's your name?");
+let lastName = prompt("What's your lastname?");
+let age = prompt("What's your age?");
+
+console.log(`Your full name is ${firstName} ${lastName}`);
+console.log(`You are ${age} years old`);
