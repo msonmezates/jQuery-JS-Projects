aa9c8e9e1103750038da611d2cd1dd3ca94b66aa
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-12-25 11:17:16 -0600

    Add more functionality for todo list

--- JavaScript_Basics/list.js
+++ JavaScript_Basics/list.js
@@ -4,12 +4,32 @@ let input = prompt('What would you like to do?');
 
 while(input !== "quit") {
 	if(input === "list") {
-		console.log(todos);
+		listTodos();	
 	} else if(input === "new") {
-		let todo = prompt("Enter new todo")
-		todos.push(todo);
-		console.log(todos);
+		newTodo();
+	} else if (input === 'delete') {
+		deleteTodo();
 	}
 	input = prompt('What would you like to do?');
 }
 console.log("OK, YOU QUIT THE APP!")
+
+function listTodos() {
+	console.log('***********');
+	todos.forEach((todo,i) => {
+		console.log(i + ': ' + todo);
+	}); 
+	console.log('***********');
+}
+
+function newTodo() {
+	let todo = prompt("Enter new todo");
+	todos.push(todo);
+	console.log('added a todo');
+}
+
+function deleteTodo() {
+	let index = prompt('Enter index of todo to delete');
+	todos.splice(index,1);
+	console.log('Deleted one todo item');
+}
