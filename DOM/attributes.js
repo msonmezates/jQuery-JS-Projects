befb2bd7bf7cbc0fad1f93bc4e9f93d0ecac01a2
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-12-27 23:47:29 -0600

    Change an image with attributes

---
+++ DOM/attributes.js
@@ -0,0 +1,5 @@
+let image1 = document.getElementsByTagName('img')[0];
+console.log(image1);
+
+console.log(image1.getAttribute('src'));
+console.log(image1.setAttribute("src","http://angrytorro.com/fs/i/2013/05/19/3a489d322613d30c5286d8196597a0.jpg"));
