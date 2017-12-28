a4573029dea59a8fb8edfb8663c935bf9143f960
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-12-27 23:53:15 -0600

    Manipulate href attribute

--- DOM/attributes.js
+++ DOM/attributes.js
@@ -3,3 +3,14 @@ console.log(image1);
 
 console.log(image1.getAttribute('src'));
 console.log(image1.setAttribute("src","http://angrytorro.com/fs/i/2013/05/19/3a489d322613d30c5286d8196597a0.jpg"));
+
+let a = document.querySelector('a');
+console.log(a);
+
+a.setAttribute('href','http://www.corgis.com');
+console.log(a);
+
+a.textContent = 'LINK TO CORGIS!';
+console.log(a);
+
+
