a951f123e2cb909f8ee9098259657cb7a8128e26
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-01-02 07:11:01 -0600

    Finish score keeper

--- DOM/scoreKeeper.js
+++ DOM/scoreKeeper.js
@@ -3,13 +3,17 @@ let p2Button = document.querySelector('#p2');
 let p1Score = 0, p2Score = 0;
 let p1Display = document.querySelector('#p1Display');
 let p2Display = document.querySelector('#p2Display');
+let resetButton = document.getElementById('reset');
+let numInput = document.querySelector('input[type="number"]');
+let winningScoreDisplay = document.querySelector('p span');
 let gameOver = false;
-let winningScore = 5;
+let winningScore = numInput.value;
 
 p1.addEventListener('click',()=> {
   if(!gameOver) {
     p1Score++;
     if(p1Score === winningScore) {
+      p1Display.classList.add('winner');
       gameOver = true;
     }
   }
@@ -20,8 +24,29 @@ p2.addEventListener('click',()=> {
   if(!gameOver) {
     p2Score++;
     if(p2Score === winningScore) {
+      p2Display.classList.add('winner');
       gameOver = true;
     }
   }
   p2Display.textContent = p2Score;
+});
+
+resetButton.addEventListener('click',()=>{
+  reset();
+});
+
+reset = () => {
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = 0;
+  p2Display.textContent = 0;
+  p1Display.classList.remove('winner');
+  p2Display.classList.remove('winner');
+  gameOver = false;
+}
+
+numInput.addEventListener('change',()=> {
+  winningScoreDisplay.textContent = numInput.value;
+  winningScore = Number(numInput.value);
+  reset();
 });
