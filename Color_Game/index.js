e3f8f81e505f5a3835013349f92ec44a922da541
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-01-02 21:49:52 -0600

    Add a reset button and reset functionality

--- Color_Game/index.js
+++ Color_Game/index.js
@@ -5,6 +5,7 @@ let pickedColor = pickColor();
 let colorDisplay = document.getElementById('colorDisplay');
 let messageDisplay = document.querySelector('#message');
 let h1 = document.querySelector('h1');
+let resetButton = document.getElementById('reset');
 
 colorDisplay.textContent = pickedColor;
 
@@ -20,6 +21,7 @@ for(let i=0; i<squares.length; i++) {
       messageDisplay.textContent = 'Correct';
       changeColors(clickedColor);
       h1.style.backgroundColor = clickedColor;
+      resetButton.textContent = 'Play Again?'
     } else {
       squares[i].style.backgroundColor = '#232323';
       messageDisplay.textContent = 'Try Again!';
@@ -60,3 +62,17 @@ function randomColor() {
   // we want to return random "rgb(r, g, b)" and 
   return "rgb(" + r + ', ' + g + ', ' + b + ")";
 }
+
+resetButton.addEventListener('click', function() {
+  //generate all new colors
+  colors = generateRandomColors(6);
+  //pick a new random color from array
+  pickedColor = pickColor();
+  //change colorDisplay to matched picked color
+  colorDisplay.textContent = pickedColor;
+  //change colors of squares
+  for(let i=0; i<squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+  }
+  h1.style.backgroundColor = '#232323';
+});
