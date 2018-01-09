634472118d9485050aa7977361899cc7adc7b0e2
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-01-09 09:32:18 -0600

    Change attributes with attr() method

--- jQuery/methods.js
+++ jQuery/methods.js
@@ -1,32 +1,43 @@
-$('h1')[0];
-// <h1>​jQuery Methods Demo Page​</h1>​
+// $('h1')[0];
+// // <h1>​jQuery Methods Demo Page​</h1>​
 
-$('h1').text()
-// jQuery Methods Demo Page
+// $('h1').text()
+// // jQuery Methods Demo Page
 
-$('ul').text()
-// 
-// 		Skittles
-// 		Starburst
-// 		Twix
-// 	
+// $('ul').text()
+// // 
+// // 		Skittles
+// // 		Starburst
+// // 		Twix
+// // 	
 
-$('li').text()
-// "SkittlesStarburstTwix"
+// $('li').text()
+// // "SkittlesStarburstTwix"
 
-$('h1').text('Assign a new text')
-$('h1').text('jQuery Methods Demo Page')
-// "Assign a new text"
-$('li:first-child').text('bonibon')
-$('li:first-child').text('Skittles')
+// $('h1').text('Assign a new text')
+// $('h1').text('jQuery Methods Demo Page')
+// // "Assign a new text"
+// $('li:first-child').text('bonibon')
+// $('li:first-child').text('Skittles')
 
-$('ul').html()
-// "
-// 		<li>Skittles</li>
-// 		<li>Starburst</li>
-// 		<li>Twix</li>
-// 	"
+// $('ul').html()
+// // "
+// // 		<li>Skittles</li>
+// // 		<li>Starburst</li>
+// // 		<li>Twix</li>
+// // 	"
 
-$('ul').html('<li>I hacked your UL</li><li>I did it again!</li>')
+// $('ul').html('<li>I hacked your UL</li><li>I did it again!</li>')
 
-$('li').html('<a href="google.com">Click Me</a>')
+// $('li').html('<a href="google.com">Click Me</a>')
+
+//attr() method
+$('input').attr('type') //outputs text
+$('input').attr('type','color') //changes input to color type
+$('input').attr('type','checkbox') //changes to checkbox
+$('input').attr('type','text') //go back to text type
+
+$('img').css('width','200px') //change img sizes to 200px
+$('img:first-of-type').css('height','300px') //change the first image's height
+$('img:first-of-type').css('src','http://www.whatever') //we can change the source
+$('img').last().css('width','100px') // we can target last element with last()
