9dc3a829a17d8bbe0a40ee66bdc2470c17dd520d
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-01-09 19:52:33 -0600

    Use click() event with this keyword

---
+++ jQuery/events.js
@@ -0,0 +1,12 @@
+//click()
+
+// $('h1').click(()=>alert('h1 is clicked')) //outputs alert to the screen
+
+// $('button').click(function(){  //change background to green when button clicked
+//   $(this).css('background','green')
+// })
+
+// $('button').click(function(){  //This gives the text when button is clicked
+//   let text = $(this).text();
+//   console.log(text)
+// })
