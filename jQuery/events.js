0cf567afec17996d04e9f299e0c7fda2e65ed678
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-01-09 20:09:35 -0600

    keypress() event

--- jQuery/events.js
+++ jQuery/events.js
@@ -1,4 +1,4 @@
-//click()
+//click() event
 
 // $('h1').click(()=>alert('h1 is clicked')) //outputs alert to the screen
 
@@ -10,3 +10,17 @@
 //   let text = $(this).text();
 //   console.log(text)
 // })
+
+
+//keypress() event
+// $('input').keypress(()=>console.log('you pressed a key'));  //it outputs the text every time a key is pressed
+
+// $('input').keypress(e=>{ //Here event object gives us key values. In this case, we can use event.which
+//   console.log(e);
+// });
+
+// $('input').keypress(e=>{  //if user presses enter, console.log
+//   if(e.which === 13) {
+//     alert('you hit enter')  
+//   }
+// });
