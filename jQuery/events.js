2eeeb89d4e9af9306dcc6009264d654279bf09d9
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-01-09 20:26:06 -0600

    Use on() method with many other events

--- jQuery/events.js
+++ jQuery/events.js
@@ -13,6 +13,7 @@
 
 
 //keypress() event
+
 // $('input').keypress(()=>console.log('you pressed a key'));  //it outputs the text every time a key is pressed
 
 // $('input').keypress(e=>{ //Here event object gives us key values. In this case, we can use event.which
@@ -24,3 +25,25 @@
 //     alert('you hit enter')  
 //   }
 // });
+
+
+//on() event
+
+// $('h1').on('click',function(){ //change color to red when h1 is clicked
+//   $('h1').css('color','red')  //this is problematic because it applies to all h1s
+// })  //therefore we need to use this keyword to change one item at a time
+
+// $('h1').on('click',function(){
+//   $(this).css('color','red')
+// });
+
+// $('input').on('keypress',function(){  //using keypress() with on() method
+//   console.log('key pressed')
+// })
+
+// $('button').on('mouseenter',function(){  //when we hover over, the font changes to bold but it stays as bold
+//   $(this).css('font-weight','bold')
+// })
+// $('button').on('mouseleave',function(){ //when we use mouseleave(), font goes back to normal so it adds toggling 
+//   $(this).css('font-weight','normal')
+// })
