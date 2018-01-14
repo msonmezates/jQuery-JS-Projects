ec86c04d89fc2fc2520654166f9578f0b737b41c
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-01-14 16:14:52 -0600

    Add create todo item functionality

--- Todo_List/assets/js/todos.js
+++ Todo_List/assets/js/todos.js
@@ -1,12 +1,20 @@
 //Check off specific Todos By Clicking
-$('li').click(function(){
+$('ul').on('click', 'li', function(){
 	$(this).toggleClass("completed");
 });
 
 //Click on X to delete Todo
-$('span').click(function(event){
+$('ul').on('click', 'span', function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation(); //stops bubbling up
+});
+
+$('input[type="text"]').keypress(function(e){
+	if (e.which === 13) {
+		let todoText =  $(this).val();
+		$(this).val('');
+		$('ul').append(`<li><span>X</span> ${todoText}</li>`);
+	}
 });
