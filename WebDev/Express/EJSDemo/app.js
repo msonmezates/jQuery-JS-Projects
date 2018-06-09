3966fbe59bd13250a0be60058de98da80b5b68df
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-06-09 13:43:39 -0500

    Add conditionals/loops for ejs files in express

--- WebDev/Express/EJSDemo/app.js
+++ WebDev/Express/EJSDemo/app.js
@@ -10,4 +10,14 @@ app.get("/fallinlovewith/:thing", (req,res) => {
   res.render("love.ejs", {thingVar: thing}); // To pass thing in to love.ejs file, we need to use an object. thingvar is our variable inside love.ejs file.
 });
 
+app.get("/posts", (req,res) => {
+  const posts = [
+    {title:'title1', author: 'author1'},
+    {title:'title2', author: 'author2'},
+    {title:'title3', author: 'author3'},
+  ];
+
+  res.render("posts.ejs",{posts});
+});
+
 app.listen(process.env.PORT || 3000, () => console.log("Server has started..."));
