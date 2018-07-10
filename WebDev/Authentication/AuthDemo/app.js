4ad0a8cd28c180ca737159711794c985bfb4448a
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-07-10 09:59:22 -0500

    Implement signup functionality for the demo

--- WebDev/Authentication/AuthDemo/app.js
+++ WebDev/Authentication/AuthDemo/app.js
@@ -16,6 +16,7 @@ app.use(require('express-session')({
 }));
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -23,12 +24,31 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// ROUTES
+
 app.get('/', (req, res) => {
   res.render('home');
 });
 
 app.get('/secret', (req, res) => {
   res.render('secret');
+});
+
+// Auth Routes
+
+// Show Signup form
+app.get('/register', (req,res) => {
+  res.render('register');
+});
+// Handle User Signup
+app.post('/register', (req,res) => {
+  const { username, password } = req.body;
+  User.register(new User({ username }), password, (err, user) => {
+    if(err) console.log(err);
+    passport.authenticate('local')(req, res, () => { // we are using local strategy
+      res.redirect('/secret');
+    });
+  });
 });
 
 app.listen(process.env.PORT || 3000, () => console.log('Server has started...'));
